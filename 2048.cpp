26b1c280829be073aa24d8cf19ccc3781368d9db
git-cheegitcheegoo <125455339+git-cheegitcheegoo@users.noreply.github.com>
2024-05-24 12:39:43 +0700

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -2,13 +2,104 @@
 #include<conio.h>
 using namespace std;
 
+void addblock(int a[4][4])
+{
+	int xi,yi;
+	srand(time(0));
+	while(1)
+	{
+		xi=rand()%4;
+		yi=rand()%4;
+		if(a[xi][yi]==0)
+		{
+			a[xi][yi]=pow(2,xi%2 + 1);
+			break;
+		}
+	}
+
+}
+
+void move_right(int a[4][4])
+{
+	int xi,yi;
+	for(int i=0;i<4;i++)
+	{
+		xi=i,yi=3;
+		for(int j=2;j>=0;j--)
+		{
+			if(a[i][j]!=0)
+			{
+				if(a[i][j+1]==0 || a[i][j+1]==a[i][j])
+				{
+					if(a[xi][yi]==a[i][j])
+					{
+						a[xi][yi]*=2;
+						a[i][j]=0;
+					}
+					else
+					{
+						if(a[xi][yi]==0)
+						{
+							a[xi][yi]=a[i][j];
+							a[i][j]=0;
+						}
+						else
+						{
+							a[xi][--yi]=a[i][j];
+							a[i][j]=0;
+						}
+					}
+				}
+				else yi--;
+			}
+		}
+	}
+}
+
+void move_left(int a[4][4])
+{
+	int xi,yi;
+	for(int i=0;i<4;i++)
+	{
+		xi=i,yi=0;
+		for(int j=1;j<4;j++)
+		{
+			if(a[i][j]!=0)
+			{
+				if(a[i][j-1]==0 || a[i][j-1]==a[i][j])
+				{
+					if(a[xi][yi]==a[i][j])
+					{
+						a[xi][yi]*=2;
+						a[i][j]=0;
+					}
+					else
+					{
+						if(a[xi][yi]==0)
+						{
+							a[xi][yi]=a[i][j];
+							a[i][j]=0;
+						}
+						else
+						{
+							a[xi][++yi]=a[i][j];
+							a[i][j]=0;
+						}
+					}
+				}
+				else yi++;
+			}
+		}
+	}
+}
+
 void move_up(int a[4][4])
 {
-	int i,j,xi,yi;
-	for(j=0;j<4;j++)
+	int xi,yi;
+	for(int j=0;j<4;j++)
 	{
 		xi=0,yi=j;
-		for(i=1;i<4;i++)
+		for(int i=1;i<4;i++)
 		{
 			if(a[i][j]!=0)
 			{
@@ -41,11 +132,11 @@ void move_up(int a[4][4])
 
 void move_down(int a[4][4])
 {
-	int i,j,xi,yi;
-	for(j=0;j<4;j++)
+	int xi,yi;
+	for(int j=0;j<4;j++)
 	{
 		xi=3,yi=j;
-		for(i=2;i>=0;i--)
+		for(int i=2;i>=0;i--)
 		{
 			if(a[i][j]!=0)
 			{
@@ -76,106 +167,61 @@ void move_down(int a[4][4])
 	}
 }
 
-void move_left(int a[4][4])
+int move_approval(int tmp[4][4],int a[4][4])
 {
-	int i,j,xi,yi;
-	for(i=0;i<4;i++)
-	{
-		xi=i,yi=0;
-		for(j=1;j<4;j++)
-		{
-			if(a[i][j]!=0)
-			{
-				if(a[i][j-1]==0 || a[i][j-1]==a[i][j])
-				{
-					if(a[xi][yi]==a[i][j])
-					{
-						a[xi][yi]*=2;
-						a[i][j]=0;
-					}
-					else
-					{
-						if(a[xi][yi]==0)
-						{
-							a[xi][yi]=a[i][j];
-							a[i][j]=0;
-						}
-						else
-						{
-							a[xi][++yi]=a[i][j];
-							a[i][j]=0;
-						}
-					}
-				}
-				else yi++;
+	int flag=1;
+	for(int i=0;i<4;i++)
+    	for(int j=0;j<4;j++)
+    		if(tmp[i][j]!=a[i][j])
+    		{
+    			flag=0;
+    			break;
 			}
-		}
-	}
+	return flag;
 }
 
-void move_right(int a[4][4])
+int movability_checker(int a[4][4])
 {
-	int i,j,xi,yi;
-	for(i=0;i<4;i++)
-	{
-		xi=i,yi=3;
-		for(j=2;j>=0;j--)
-		{
-			if(a[i][j]!=0)
-			{
-				if(a[i][j+1]==0 || a[i][j+1]==a[i][j])
-				{
-					if(a[xi][yi]==a[i][j])
-					{
-						a[xi][yi]*=2;
-						a[i][j]=0;
-					}
-					else
-					{
-						if(a[xi][yi]==0)
-						{
-							a[xi][yi]=a[i][j];
-							a[i][j]=0;
-						}
-						else
-						{
-							a[xi][--yi]=a[i][j];
-							a[i][j]=0;
-						}
-					}
-				}
-				else yi--;
+	int blank=0,merg=0;
+	for(int i=0;i<4;i++)
+    	for(int j=0;j<4;j++)
+    		if(a[i][j]==0)
+    		{
+    			blank=1;
+				break;
 			}
-		}
-	}
+
+	for(int i=0;i<3;i++)
+    	for(int j=0;j<3;j++)
+    		if(a[i+1][j]==a[i][j] || a[i][j+1]==a[i][j])
+    		{
+    			merg=1;
+    			break;
+			}
+
+	if(blank || merg) return 1;
+	else return 0;
 }
 
-void addblock(int a[4][4])
+int win_check(int a[4][4])
 {
-	int xi,yi;
-	srand(time(0));
-	while(1)
-	{
-		xi=rand()%4;
-		yi=rand()%4;
-		if(a[xi][yi]==0)
-		{
-			a[xi][yi]=pow(2,xi%2 + 1);
-			break;
-		}
-	}
-
+    for(int i=0;i<4;i++)
+    	for(int j=0;j<4;j++)
+    		{
+    		    if (a[i][j] >= 2048)
+                return 1;
+    		}
+    return 0;
 }
 
 void ui(int a[4][4])
 {
 	cout<<"\n\t\t\tPress ESC to quit the game";
 	cout<<"\n\n\n";
-	int i,j;
-	for(i=0;i<4;i++)
+	for(int i=0;i<4;i++)
 	{
 		cout<<"\t\t\t-------------------------\n\t\t\t";
-		for(j=0;j<4;j++)
+		for(int j=0;j<4;j++)
 		{
 			if(a[i][j]==0) cout<<"|     ";
 			else
@@ -189,43 +235,6 @@ void ui(int a[4][4])
 	}
 	cout<<"\t\t\t------------------------\n";
 }
-
-int move_approval(int tmp[4][4],int a[4][4])
-{
-	int fl=1,i,j;
-	for(i=0;i<4;i++)
-    	for(j=0;j<4;j++)
-    		if(tmp[i][j]!=a[i][j])
-    		{
-    			fl=0;
-    			break;
-			}
-	return fl;
-}
-
-int movability_checker(int a[4][4])
-{
-	int fl=0,gl=0,i,j;
-	for(i=0;i<4;i++)
-    	for(j=0;j<4;j++)
-    		if(a[i][j]==0)
-    		{
-    			fl=1;
-				break;
-			}
-
-	for(i=0;i<3;i++)
-    	for(j=0;j<3;j++)
-    		if(a[i+1][j]==a[i][j] || a[i][j+1]==a[i][j])
-    		{
-    			gl=1;
-    			break;
-			}
-
-	if(fl || gl) return 1;
-	else return 0;
-}
-
 int main()
 {
 	cout<<"\n\n\n\n\t\t\t2048 GAME\n\n\n\t\tPress any key to continue";
@@ -236,6 +245,7 @@ int main()
 	srand(time(0));
 	i1=rand()%4;
 	i2=rand()%4;
+	bool checkp = true;
 	while(1)
 	{
 		i3=rand()%4;
@@ -269,6 +279,13 @@ int main()
 			cout<<"\n\n\t\t\tGAME OVER!!\n\n\n";
 			getch();
 			break;
+		}
+		if(win_check(board) && checkp == true)
+		{
+			cout<<"\n\n\t\t\t\tYOU WIN!!\n\n\n";
+            cout<<"\t\t\tContinue playing or ESC to quit";
+			getch();
+			checkp = false;
 		}
 	}
 	return 0;
